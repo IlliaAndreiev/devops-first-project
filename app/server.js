6370f9d0f744097d3d58d97519075a8dcb200de3
IlliaAndreiev <iandreiev@gmail.com>
2026-05-16 11:21:36 +0200

    Initial DevOps project

---
+++ app/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { Client } = require("pg");
+
+const app = express();
+const PORT = 3000;
+
+const client = new Client({
+  host: "postgres",
+  user: "devops",
+  password: "devops",
+  database: "devopsdb",
+  port: 5432,
+});
+
+client.connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch(err => console.error("PostgreSQL connection error", err));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await client.query("SELECT NOW()");
+    
+    res.json({
+      message: "Hello DevOps",
+      database_time: result.rows[0].now
+    });
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).send("Database error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
